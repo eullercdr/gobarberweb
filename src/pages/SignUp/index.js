5f46f86b39cf4b0d834d6fbe1745ed15363f69c3
Euller Cristian <eullercdr@gmail.com>
2020-03-02 17:18:28 -0300

    Validações de Formulário

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  name: Yup.string().required('O Nome é obrigatorio'),
+  email: Yup.string()
+    .email('Insira um e-mail valido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'A senha deve ter 6 caracteres')
+    .required('A senha é obrigatoria'),
+});
 
 // import { Container } from './styles';
 
@@ -14,7 +25,7 @@ export default function SignUp() {
   return (
     <>
       <img src={logo} alt="Gobarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
